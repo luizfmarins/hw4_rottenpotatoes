620f9bff4298a34f5f5598b0dd0ff1798f30143e
luizfmarins <marins@marinsLinuxPC.(none)>
2013-06-04 00:05:45 -0300

    Iniciando testes com Cucumber

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -1,27 +1,12 @@
-# TL;DR: YOU SHOULD DELETE THIS FILE
-#
-# This file is used by web_steps.rb, which you should also delete
-#
-# You have been warned
 module NavigationHelpers
-  # Maps a name to a path. Used by the
-  #
-  #   When /^I go to (.+)$/ do |page_name|
-  #
-  # step definition in web_steps.rb
-  #
   def path_to(page_name)
     case page_name
 
     when /^the home\s?page$/
       '/'
-
-    # Add more mappings here.
-    # Here is an example that pulls values out of the Regexp:
-    #
-    #   when /^(.*)'s profile page$/i
-    #     user_profile_path(User.find_by_login($1))
-
+    
+    when /^details page for \".+"$/ then '/movies'  
+    
     else
       begin
         page_name =~ /^the (.*) page$/
